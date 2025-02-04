876aa8bc24c69733c9de4c499c73312c31c92133
3NiTiN3 <161741906+3NiTiN3@users.noreply.github.com>
2025-02-04 16:38:15 +0530

    analyze

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
 import ProductGrid from "@/components/ProductGrid/ProductGrid";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main>
+      <Analytics/>
       <Hero />
       <ProductGrid />
     </main>
